e53b8854d930ff9da6d98192d4ce5c9a89aee530
RRKRAJESH <kumarrajesh984000@gmail.com>
2022-12-29 04:42:20 +0530

    first push to master

---
+++ src/test/java/com/practice/PracticeClass.java
@@ -0,0 +1,14 @@
+package com.practice;
+
+public class PracticeClass {
+
+	public static void main(String[] args) {
+
+		int a[] = { 10, 20, 30, 40, 50 };
+		int b[] = { 40, 50, 60, 70, 80 };
+
+	
+	
+	}
+
+}
