7f1260fca30f0e055b62d65e3364833c10ac180a
RRKRAJESH <kumarrajesh984000@gmail.com>
2022-12-29 05:55:36 +0530

    code send to rajesh branch

--- src/test/java/com/practice/PracticeClass.java
+++ src/test/java/com/practice/PracticeClass.java
@@ -6,6 +6,18 @@ public class PracticeClass {
 
 		int a[] = { 10, 20, 30, 40, 50 };
 		int b[] = { 40, 50, 60, 70, 80 };
+		
+		for (int i = 0; i < a.length; i++) {
+			
+			for (int j = 0; j < b.length; j++) {
+				
+				if (a[i]==b[i]) {
+					
+				}
+				
+			}
+			
+		}
 
 	
 	
